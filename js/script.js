9cd3118e171bf94e972d6838bc19b85c08ad66ac
ibragimKariev <ibrosh_96@mail.ru>
2022-04-06 09:52:53 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+let menuOpen = false;
+menuBtn.addEventListener('click', ()=> {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menu.classList.add('active');
+        menuOpen=true;
+    }
+    else {
+        menuBtn.classList.remove('open');
+        menu.classList.remove('active');
+        menuOpen=false;
+    }
+})
+const pp = document.querySelector('.pp_block');
+const open_pp = document.querySelector('#open_pp');
+const close_pp = document.querySelector('.bg_overlay');
+
+open_pp.addEventListener('click', ()=> {
+    pp.classList.add('shown');
+})
+close_pp.addEventListener('click', ()=> {
+    pp.classList.remove('shown');
+})
